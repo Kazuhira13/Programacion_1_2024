098b4585da0f4a7f68e4e5b8f78ae942148135a1
kaz <tgpp1312@gmail.com>
2024-03-02 10:53:36 -0600

    Examen parcial

---
+++ PrimerParcial/Parcial 1 serie II Ejerc2/Parcial 1 serie II Ejerc2/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class program
+{
+    static void Main(string[] args)
+    {
+        int[] numeros = new int[4];
+        int sumaPares = 0;
+        int sumaImpares = 0;
+         for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine("Ingrese los datos",i + 1);
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("numeros pares");
+        foreach (int num in numeros)
+        {
+            if (num % 2 == 0)
+            {
+                sumaPares += num;
+                Console.WriteLine(num);
+            }
+        }
+
+        Console.WriteLine("numeros impares");
+        foreach (int num in numeros)
+        {
+            if (num % 2 == 1)
+            {
+                sumaImpares += num;
+                Console.WriteLine(num);
+            }
+        }
+        Console.WriteLine("Suma de numeros pares:" + sumaPares);
+        Console.WriteLine("Suma de numeros impares:" +sumaImpares);
+
+ 
+
+
+    }
+}
---
+++ PrimerParcial/Parcial 1 variante A Serie II ejeR1/Parcial 1 variante A Serie II ejeR1/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_1_variante_A_Serie_II_ejeR1
+{
+    internal class Calculadora
+    {
+        public int suma(int x1, int x2)
+        {
+            return x1 + x2;
+        }
+
+        public int resta(int x1, int x2)
+        {
+            return x1 - x2;
+        }
+
+        public int Multiplicacion(int x1, int x2)
+        {
+            return x1 * x2;
+        }
+
+        public int Division(int x1, int x2)
+        {
+            return x1 / x2;
+        }
+    }
+}
