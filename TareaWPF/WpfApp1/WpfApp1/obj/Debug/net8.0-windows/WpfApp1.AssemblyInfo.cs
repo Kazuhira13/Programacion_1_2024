a0b915888f37f3fb8614f61d303f26faa706267b
kaz <tgpp1312@gmail.com>
2024-04-27 10:41:25 -0600

    actualizacion

--- TareaWPF/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ TareaWPF/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d0c911b086a9211ce996b0c7bf16bbd65950ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
