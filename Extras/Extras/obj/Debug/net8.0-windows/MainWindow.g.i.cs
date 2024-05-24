1a09a9c35a33cbd2f5683fa93de76ea7b5c2a184
kaz <tgpp1312@gmail.com>
2024-05-23 22:11:01 -0600

    actualizacion

--- Extras/Extras/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Extras/Extras/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -64,7 +64,7 @@ namespace Extras {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Extras;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Extras;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
